c35e8dbd7a34fdb592c4d6fbecb084093e745b9c
Anastasia Gerasimova <anastasiagerasimova210295@gmail.com>
2020-10-30 02:33:35 +0300

    Updates

---
+++ precache-manifest.691bed089f3e2ff2df2f821b8b45a527.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3b45f5437f48b1ee6bc6cc171c5b4e",
+    "url": "/re-store-react/index.html"
+  },
+  {
+    "revision": "ce4c69ceebb2a6159a5b",
+    "url": "/re-store-react/static/css/main.b2dd53a5.chunk.css"
+  },
+  {
+    "revision": "b10481be3fcab21dbf08",
+    "url": "/re-store-react/static/js/2.7a3bf374.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/re-store-react/static/js/2.7a3bf374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce4c69ceebb2a6159a5b",
+    "url": "/re-store-react/static/js/main.d53307ec.chunk.js"
+  },
+  {
+    "revision": "ecbe555f0b2404888f5f",
+    "url": "/re-store-react/static/js/runtime-main.0c3ee770.js"
+  }
+]);
